6c8446620e0cb43eaf7a1568a92a122b01c3e31c
AgusVallejos <agustinvallejos010@gmail.com>
2022-07-06 21:14:26 -0300

    Cambios

--- rand.php
+++ rand.php
@@ -6,6 +6,6 @@ error_reporting(E_ALL);
 
 $valor = rand(1, 5);
 
-echo $valor == 1 || $valor == 3 || $valor == 5? "El número es impar" : "El número es par";
+echo $valor == 1 || $valor == 3 || $valor == 5? "El número $valor es impar" : "El número $valor es par";
 
 ?>
